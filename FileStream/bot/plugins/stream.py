c72c1f6e0995837356d5994bed6a10e4db26cad5
oldcaartoonstamil <149665026+oldcaartoonstamil@users.noreply.github.com>
2024-04-01 22:38:36 +0530

    Update stream.py

--- FileStream/bot/plugins/stream.py
+++ FileStream/bot/plugins/stream.py
@@ -1,6 +1,6 @@
 
 import asyncio
-from FileStream.bot import FileStream, multi_clients
+from FileStream.bot import FileStream, multi_clients, app
 from FileStream.utils.bot_utils import is_user_banned, is_user_exist, is_user_joined, gen_link, is_channel_banned, is_channel_exist, is_user_authorized
 from FileStream.utils.database import Database
 from FileStream.utils.file_properties import get_file_ids, get_file_info
