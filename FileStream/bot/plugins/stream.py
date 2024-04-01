a0ccb336c88474d23b5546f298b53562367aa137
oldcaartoonstamil <149665026+oldcaartoonstamil@users.noreply.github.com>
2024-04-01 22:53:22 +0530

    Update stream.py

--- FileStream/bot/plugins/stream.py
+++ FileStream/bot/plugins/stream.py
@@ -42,7 +42,7 @@ async def private_receive_handler(bot: Client, message: Message):
     fn3=[]
     async for MSG in app.search_messages(chat_id=-1001990899694, query=str(file_name1),offset=skip_no, limit=limit_no, filter=filter):
         msg = await bot.get_messages(-1001990899694, MSG.id)
-        file2=getattr(msg)
+        file2=getattr(msg,msg.media.value)
         fn3.append(str(file2.file_name))
     if file_name1 not in fn3:
         return
