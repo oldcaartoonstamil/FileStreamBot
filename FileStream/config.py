7868277036c6d293ef4007f352f874350e83c5e7
oldcaartoonstamil <149665026+oldcaartoonstamil@users.noreply.github.com>
2024-04-01 18:31:19 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Telegram:
-    API_ID = int(env.get("API_ID",29245668))
-    API_HASH = str(env.get("API_HASH",'53f6d669e7f3e872a00a65dc0686eeac'))
-    BOT_TOKEN = str(env.get("BOT_TOKEN",'6455795402:AAECZkl17Y8TK_H8SPt947tjame4ZaQ8-lM'))
-    OWNER_ID = int(env.get('OWNER_ID', '1328284557'))
+    API_ID = int(env.get("API_ID"))
+    API_HASH = str(env.get("API_HASH"))
+    BOT_TOKEN = str(env.get("BOT_TOKEN"))
+    OWNER_ID = int(env.get('OWNER_ID', '7978482443'))
     WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    DATABASE_URL = str(env.get('DATABASE_URL','mongodb+srv://oldfilestorebot:ofsb@cluster0.myqdttg.mongodb.net/?retryWrites=true&w=majority'))
+    DATABASE_URL = str(env.get('DATABASE_URL'))
     UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Telegram"))
-    SESSION_NAME = str(env.get('SESSION_NAME', 'filestreammedialink'))
+    SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
     FORCE_SUB_ID = env.get('FORCE_SUB_ID', None)
     FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', False)
     FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
@@ -20,8 +20,8 @@ class Telegram:
     START_PIC = env.get('START_PIC', "https://graph.org/file/290af25276fa34fa8f0aa.jpg")
     VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
     MULTI_CLIENT = False
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", -1001806967645))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", -1001806967645))   # Logs channel for user logs
+    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", None))   # Logs channel for file logs
+    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", None))   # Logs channel for user logs
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
@@ -29,8 +29,10 @@ class Telegram:
 class Server:
     PORT = int(env.get("PORT", 8080))
     BIND_ADDRESS = str(env.get("BIND_ADDRESS", "0.0.0.0"))
-    PING_INTERVAL = int(env.get("PING_INTERVAL", "300"))
+    PING_INTERVAL = int(env.get("PING_INTERVAL", "1200"))
     HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
     NO_PORT = str(env.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
-    FQDN = str(env.get("FQDN",'medialinksstreaj-2072707f9998.herokuapp.com'))
-    URL = "https://{}/".format(FQDN)
+    FQDN = str(env.get("FQDN", BIND_ADDRESS))
+    URL = "http{}://{}{}/".format(
+        "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
+    )
