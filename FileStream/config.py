35fdb672a9852c2bd66eeb175085770b91be775f
oldcaartoonstamil <149665026+oldcaartoonstamil@users.noreply.github.com>
2024-04-02 16:33:49 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -6,12 +6,12 @@ load_dotenv()
 class Telegram:
     API_ID = 29245668
     API_HASH = "53f6d669e7f3e872a00a65dc0686eeac"
-    BOT_TOKEN = "6455795402:AAECZkl17Y8TK_H8SPt947tjame4ZaQ8-lM"
+    BOT_TOKEN = "6977362228:AAHPSg0vAcW9g57Z8ZjcBdJzOIDrYofqud8"
     OWNER_ID = 1328284557
     WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
     DATABASE_URL = "mongodb+srv://oldfilestorebot:ofsb@cluster0.myqdttg.mongodb.net/?retryWrites=true&w=majority"
     UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "media_links"))
-    SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream112'))
+    SESSION_NAME = str(env.get('SESSION_NAME', 'filestream'))
     FORCE_SUB_ID = -1001304669315
     FORCE_SUB = True
     SLEEP_THRESHOLD = int(env.get("SLEEP_THRESHOLD", "60"))
@@ -19,11 +19,12 @@ class Telegram:
     START_PIC = env.get('START_PIC', "https://telegra.ph/file/7de19fc3f09d19ddbf823.jpg")
     VERIFY_PIC = env.get('VERIFY_PIC', "https://telegra.ph/file/7de19fc3f09d19ddbf823.jpg")
     MULTI_CLIENT = False
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", -1001806967645))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", -1001806967645))   # Logs channel for user logs
+    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", -1002027926399))   # Logs channel for file logs
+    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", -1002027926399))   # Logs channel for user logs
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
+    SESSION= str(env.get('SESSION_STRING', ''))
 
 class Server:
     PORT = int(env.get("PORT", 8080))
