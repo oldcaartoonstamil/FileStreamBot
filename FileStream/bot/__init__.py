e36b534f94cc1ffcdbe70aa8c6b0e1a1d2e55c3d
oldcaartoonstamil <149665026+oldcaartoonstamil@users.noreply.github.com>
2024-04-02 16:34:50 +0530

    Update __init__.py

--- FileStream/bot/__init__.py
+++ FileStream/bot/__init__.py
@@ -7,7 +7,7 @@ if Telegram.SECONDARY:
 else:    
     plugins={"root": "FileStream/bot/plugins"}
     no_updates=None
-app =  Client("USERBOT", api_id=29245668,api_hash="53f6d669e7f3e872a00a65dc0686eeac",session_string="Telegram.SESSION")
+app =  Client("USERBOT", api_id=29245668,api_hash="53f6d669e7f3e872a00a65dc0686eeac",session_string=Telegram.SESSION)
 FileStream=Client(
     name="FileStream",
     api_id=Telegram.API_ID,
