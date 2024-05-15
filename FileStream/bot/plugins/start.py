8f77a477bde7d8718871cd41ee409b3675d98548
oldcaartoonstamil <149665026+oldcaartoonstamil@users.noreply.github.com>
2024-05-15 18:35:35 +0530

    Update start.py

--- FileStream/bot/plugins/start.py
+++ FileStream/bot/plugins/start.py
@@ -43,14 +43,19 @@ async def start(bot: Client, message: Message):
                 if file_id == usr_cmd:
                     reply_markup, stream_text = await gen_linkx(m=message, _id=file_id,
                                                                 name=[FileStream.username, FileStream.fname])
-                    await message.reply_text(
-                        text=stream_text,
-                        parse_mode=ParseMode.HTML,
-                        disable_web_page_preview=True,
-                        reply_markup=reply_markup,
-                        quote=True
-                    )
-
+                    tam=await message.reply_text(
+                            text=stream_text,
+                            parse_mode=ParseMode.HTML,
+                            disable_web_page_preview=True,
+                            reply_markup=reply_markup,
+                            quote=True
+                        )
+                    tam1=await message.reply_text("**This Message will be deleted in 15 minutes to avoid copyright issues. Please forward and save them.\n\nFor bot support, contact @old_cartoonstamil.**")
+                    asyncio.create_task(delete_after_delay(tam,900))
+                    asyncio.create_task(delete_after_delay(tam1,900))
+                    async def delete_after_delay(message, delay):
+                        await asyncio.sleep(delay)
+                        await message.delete()
             except FIleNotFound as e:
                 await message.reply_text("File Not Found")
             except Exception as e:
